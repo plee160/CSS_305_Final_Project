64d741ef1577e8bb5371546433f87d020fe79a5f
plee160 <philip.hmoov.lee@gmail.com>
2025-12-02 12:54:58 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 
 <?php 
 $host = "localhost";
-$user = "root";
-$password = "mysql";
-$dbname = "css_305_final";
+$user = "plee_login";
+$password = "GrapeShot9000#";
+$dbname = "login";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection Failed T 3T" . $conn_error);
+
 }
